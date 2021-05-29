37d0628fd0b412e422693bf983a566eaaf8d7b62
guwhan.yeum <guwhan.yeum@flitto.com>
2021-05-29 16:25:03 +0900

    Message 기능 추가

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { Observable, of } from 'rxjs'
+import { Hero } from './heroes/hero'
+import { HEROES } from './heroes/mock-heroes'
+import { MessageService } from './messages/message.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes')
+    return of(HEROES)
+  }
+}
