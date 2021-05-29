0142e37924ec736a0be9de1687f35e5096eaed85
guwhan.yeum <guwhan.yeum@flitto.com>
2021-05-29 16:24:34 +0900

    Message 기능 추가

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service'
+import { MessageService } from '../messages/message.service'
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
+  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[]
   selectedHero: Hero
 
-  constructor() { }
+  constructor(
+    private readonly heroService: HeroService,
+    private readonly messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 }
