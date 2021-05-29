a033f9986727f880fb82bc9656739655d313dbfd
guwhan.yeum <guwhan.yeum@flitto.com>
2021-05-29 16:55:11 +0900

    hero 상세 및 대시보드, 라우팅, 스타일

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
+import { Location } from '@angular/common'
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { HeroService } from '../hero.service'
 import { Hero } from '../heroes/hero'
 
 @Component({
@@ -9,9 +12,23 @@ import { Hero } from '../heroes/hero'
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private readonly heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHero()
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
 }
